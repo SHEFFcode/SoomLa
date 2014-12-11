26fa3c91bacb3783b5afb16fc43f3ecb6ceb0a8a
Yuriy Shefer <sheff@sheffmachine.com>
2014-12-10 19:53:48 -0600

    Example Assets.h

---
+++ Classes/ExampleAssets.h
@@ -0,0 +1,44 @@
+//
+//  ExampleAssets.h
+//  HelloWorld2
+//
+//  Created by Yuriy Shefer on 12/10/14.
+//
+//
+
+#ifndef __HelloWorld2__ExampleAssets__
+#define __HelloWorld2__ExampleAssets__
+
+#include <stdio.h>
+#include "CCStoreAssets.h"
+#include "cocos2d.h"
+
+class ExampleAssets: public soomla::CCStoreAssets {
+    cocos2d::Array *mCurrencies;
+    cocos2d::Array *mGoods;
+    cocos2d::Array *mCurrencyPacks;
+    cocos2d::Array *mCategories;
+public:
+    ExampleAssets():
+    mCurrencies(NULL),
+    mGoods(NULL),
+    mCurrencyPacks(NULL),
+    mCategories(NULL)
+    {}
+    
+    virtual ~ExampleAssets();
+    
+    static ExampleAssets *create();
+    bool init();
+    
+    virtual int getVersion();
+    
+    virtual cocos2d::CCArray *getCurrencies();
+    
+    virtual cocos2d::CCArray *getGoods();
+    
+    virtual cocos2d::CCArray *getCurrencyPacks();
+    
+    virtual cocos2d::CCArray *getCategories();
+};
+#endif /* defined(__HelloWorld2__ExampleAssets__) */
